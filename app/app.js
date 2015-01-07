6d8f831d828e77b645b617df871ffbcdaf10a5fb
Adam Risser <arisser@gmail.com>
2015-01-06 23:15:59 -0500

    stripping out ngRouter and adding ui-router

--- app/app.js
+++ app/app.js
@@ -2,11 +2,30 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
+  'ui.router',
   'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'myApp.view2'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/view1');
+
+  $stateProvider
+    .state('state1', {
+      url: '/view1',
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl' //,
+      // onEnter: function () {
+      //   debugger;
+      // },
+      // onExit: function () {
+      //   debugger;
+      // }
+    })
+    .state('state2', {
+      url: '/view2',
+      templateUrl: 'view2/view2.html',
+      controller: 'View2Ctrl' 
+    });
+
+});
