b1687cbbab0ead0105a8d78a4fa0956778c6f29a
Adam Risser <arisser@gmail.com>
2015-01-07 23:50:36 -0500

    filtering

--- app/members/members-ctrl.js
+++ app/members/members-ctrl.js
@@ -19,4 +19,6 @@ angular.module('app.members', [])
 
   .controller('MembersCtrl', ['$scope', 'members', function ($scope, members) {
     $scope.members = members;
+    $scope.limit = 10;
+    $scope.states = ['AK','AL','AR','AZ','CA','CO','CT','DC','DE','FL','GA','GU','HI','IA','ID', 'IL','IN','KS','KY','LA','MA','MD','ME','MH','MI','MN','MO','MS','MT','NC','ND','NE','NH','NJ','NM','NV','NY', 'OH','OK','OR','PA','PR','PW','RI','SC','SD','TN','TX','UT','VA','VI','VT','WA','WI','WV','WY'];
   }]);
